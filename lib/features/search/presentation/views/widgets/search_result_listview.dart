c4f7f3928c3a960cb26191e783104db767da4ad6
raneemn <98845001+raneemn@users.noreply.github.com>
2025-01-17 04:59:26 +0300

    search view done

--- lib/features/search/presentation/views/widgets/search_result_listview.dart
+++ lib/features/search/presentation/views/widgets/search_result_listview.dart
@@ -1,14 +1,13 @@
-import 'package:bookly_app/features/home/presentation/views/widgets/book_listview_item.dart';
 import 'package:flutter/material.dart';
 
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
+import '../../../../home/presentation/views/widgets/book_listview_item.dart';
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
+      return ListView.builder(
         padding: EdgeInsets.zero,
         itemCount: 10,
         itemBuilder: (context, index) {
--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/search/presentation/views/widgets/search_result_listview.dart';
 import 'package:flutter/material.dart';
 import 'custom_search_text_field.dart';
 
@@ -7,8 +9,24 @@ class SearchViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 30,vertical: 20),
-      child: CustomSearchTextField(),
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomSearchTextField(),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Search Results',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Expanded(child: SearchResultListView()),
+        ],
+      ),
     );
   }
 }
