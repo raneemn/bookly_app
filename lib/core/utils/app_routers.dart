0b73160d7045087a9ddbaf18bccf390239c1e9cc
raneemn <98845001+raneemn@users.noreply.github.com>
2025-01-17 04:51:18 +0300

    create custom search text field

--- lib/core/utils/app_routers.dart
+++ lib/core/utils/app_routers.dart
@@ -1,16 +1,22 @@
 import 'package:bookly_app/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/features/home/presentation/views/home_view.dart';
+import 'package:bookly_app/features/search/presentation/views/search_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouters {
   static const kHomeView = '/homeView';
+  static const kSearchView = '/searchView';
   static const kBookDetailsView = '/bookDetailsView';
   static final router = GoRouter(
     routes: [
     GoRoute(
       path: '/',
       builder: (context, state) => const SplashView(),
+    ),
+     GoRoute(
+      path: kSearchView,
+      builder: (context, state) => const SearchView(),
     ),
     GoRoute(
       path: kHomeView,
---
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'custom_search_text_field.dart';
+
+class SearchViewBody extends StatelessWidget {
+  const SearchViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding:  EdgeInsets.symmetric(horizontal: 30,vertical: 20),
+      child: CustomSearchTextField(),
+    );
+  }
+}
